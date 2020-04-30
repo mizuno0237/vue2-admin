bc710c328a9d00b844de776534df9233081236ad
Yunhao Tao <taoyh2@lenovo.com>
2020-04-30 17:44:49 +0800

    Add ElementUI and hello world

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
-  <div id="app">
-    hello world
-  </div>
+    <div id="app">
+        <el-button @click="visible = true">Button</el-button>
+        <el-dialog :visible.sync="visible" title="Hello world">
+            <p>Try Element</p>
+        </el-dialog>
+    </div>
 </template>
 
 <script>
-
-export default {
-
-}
+    export default {
+         data: function() {
+                return { visible: false }
+        }
+    }
 </script>
 
 <style>
